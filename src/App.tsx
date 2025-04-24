b23698df4d66314e0467b7505fdda8e00cdb2b0b
Jhon <jhongil96@gmail.com>
2025-04-24 16:01:45 -0500

    Fixed en desarrollo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Admin } from "./components/Admin";
 
 export default function App() {
   return (
-    <BrowserRouter basename={"/rastrea-tu-envio/"}>
+    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? 'rastrea-tu-envio' : '/'}>
       <Routes>
         <Route path="/" element={<SearchForm />} />
         <Route path="/resultado/:guia" element={<TrackingResult />} />
--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? 'rastrea-tu-envio' : '.'
+  base: process.env.NODE_ENV === 'production' ? 'rastrea-tu-envio' : '/'
 })
